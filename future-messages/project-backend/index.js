141ec8e38248ae2d582646fae913ab4119298ec0
isra <iisraaa1112@gmail.com>
2025-08-06 04:13:54 +0500

    Added entry points in index.js

--- future-messages/project-backend/index.js
+++ future-messages/project-backend/index.js
@@ -1,21 +1,28 @@
+require('dotenv').config();
 
-const express = require('express');
-
-
+import express, { json } from 'express';
 const app = express();
 
+const logger = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
 
-const PORT = 3000;
-
-
-app.use(express.json());
 const messageRoutes = require('./routes/messageRoutes');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(logger);
+app.use(express.json());
+
 app.use('/api/messages', messageRoutes);
 
 app.get('/', (req, res) => {
     res.send('🚀 Future Messages backend is running!');
 });
+app.use((req, res) => {
+    res.status(404).json({ error: 'Route not found!'});
+});
 
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`✅ Server is listening on http://localhost:${PORT}`);
